7b4d72194b702e56cee0ee5272ccb37c5ca1b88b
Matt <whelan.matthew.t@gmail.com>
2025-08-05 14:30:23 +0100

    Add two new glitched memories

--- image_poem_to_html.py
+++ image_poem_to_html.py
@@ -1,19 +1,30 @@
 # This Python script takes an image/poem name, and creates the necessary html in order to insert into the html document.
 
+# It also takes the original image and adds a white border to it ready for inserting into the glitched memories page
+
+import os
+from PIL import Image
+
+poem_name = "Common sense"
+markdown_file = "poems_and_images/" + poem_name + ".md"
+
+# Check if image is jpg or png format
+if os.path.isfile("poems_and_images/" + poem_name + ".jpg"):
+    image_path = "poems_and_images/" + poem_name + ".jpg"
+else:
+    image_path = "poems_and_images/" + poem_name + ".png"
+
 def read_markdown(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
     return content
-
-poem_name = "Flower store"
-markdown_file = "poems_and_images/" + poem_name + ".md"
 
 html_text = f"""
 <div class="blog-single-post-thumb">
    <div class="blog-post-title">
        <h3 class="neuropol">{poem_name}</h3>
    </div>
-   <img src="poems_and_images/{poem_name}.jpg" class="img-responsive">
+   <img src="{image_path}" class="img-responsive">
    <h3 class="neuropol" style="font-style: italic; font-size: 10px">X</h3>\n
 """
 
@@ -49,6 +60,28 @@ html_text += """
 </div>
 """
 
-
-
 print(html_text)
+
+def add_white_background(image_path, output_path):
+    # Open the original image
+    img = Image.open(image_path).convert("RGBA")
+
+    # Get original size
+    width, height = img.size
+    max_dim = int(max(width, height) * 1.05)
+
+    # Create a white background image (square)
+    square_bg = Image.new("RGB", (max_dim, max_dim), (255, 255, 255))
+
+    # Calculate top-left position to paste the original image
+    x_offset = (max_dim + 100 - width) // 2
+    y_offset = (max_dim - height) // 2
+
+    # Paste image onto the center of white background
+    square_bg.paste(img.convert("RGB"), (x_offset, y_offset))
+
+    # Save result
+    square_bg.save(output_path)
+    print(f"Saved: {output_path}")
+
+add_white_background(image_path, image_path[:-4] + "_white_border.jpg")
---
+++ poems_and_images/Common sense.md
@@ -0,0 +1,13 @@
+The hurt renders me down and out –
+For poorest share the largest heart,
+But richest lost their own to wealth.
+
+A lion roared in jest to me:
+
+Cloaked in horror
+Striped in colour
+Cloaked in sorrow
+For tomorrow.
+
+Cloaked in waste and future tense,
+Choked to death by common sense.
